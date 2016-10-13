dc4573ac8be977e2fda284c6e44a7450053047ab
Igor Duarte Milanez Vieira <igor_recreio23@hotmail.com>
2016-10-13 11:12:06 -0300

    Add files via upload

---
+++ RSSreader/RSSreader_1/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.ServiceModel.Syndication;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace RSSReader_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Variables
+        private readonly rssreader _model;
+        private string _path;
+        #endregion
+        #region Properties
+        private rssreader Model
+        {
+            get { return _model; }
+        }
+        #endregion
+        public MainWindow()
+        {
+            _model = new rssreader();
+            WindowState = WindowState.Maximized;
+            Model.ReadUrls();
+            InitializeComponent();
+            DataContext = _model;
+
+        }
+
+        private void add_url_Click(object sender, RoutedEventArgs e)
+        {
+            Model.AddUrlOnArray();
+        }
+        private void del_url_Click(object sender, RoutedEventArgs e)
+        {
+            Model.DeleteUrlFromList();
+        }
+
+        private void Row_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            dynamic activeX = this.peel.GetType().InvokeMember("ActiveXInstance",
+                    BindingFlags.GetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+              null, this.peel, new object[] { });
+
+            activeX.Silent = true;
+            Model.ChangeDescription(feel, peel);
+        }
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new System.Diagnostics.ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+    }
+}
---
+++ RSSreader/RSSreader_1/url.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RSSReader_1
+{
+    class url
+    {
+
+        public string Author { get; set; }
+        public string Category { get; set; }
+        public string Date { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public int Index { get; set; }
+        public string HyperLink { get; set; }
+
+        public url(string titulo, string autor, string data, string categoria, string url, string description, int index ,string hyperLink)
+        {
+            Title = titulo;
+            Author = autor;
+            Date = data;
+            Category = categoria;
+            Url = url;
+            Description = description;
+            Index = index;
+            HyperLink = hyperLink;
+        }
+      
+    }
+}
